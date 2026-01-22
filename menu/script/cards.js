920a4a1d6fb11f2a08fa08cf747ed9936c865f3e
Janani1259 <janani.s@prodian.co.in>
2026-01-22 11:47:05 +0530

    Add files via upload

---
+++ menu/script/cards.js
@@ -0,0 +1,8 @@
+const cardsData = [
+  { img: "../assets/food1.jpeg", title: "Burger King", desc: "Fast Food" },
+  { img: "../assets/food2.jpg", title: "Pizza Hut", desc: "Italian" },
+  { img: "../assets/food3.png", title: "KFC", desc: "Chicken & Fast Food" },
+  { img: "../assets/food4.jpg", title: "Domino's", desc: "Pizza" },
+  { img: "../assets/food5.jpg", title: "Subway", desc: "Healthy & Sandwiches" },
+  { img: "../assets/food6.jpeg", title: "McDonald's", desc: "Burgers & Fries" }
+];
